5b08f7c939d4a81d6c200aa8a423b075d907d2f5
Christophe Guerard <christophe.guerard@intel.com>
2016-09-21 07:41:28 +0200

    build: create directory prio to use it
    Change-Id: Ibd18c0de1e7ca5508b12cf3921dbb339d66990a5
    Tracked-On: OAM-38073
    Signed-off-by: Christophe Guerard <christophe.guerard@intel.com>
    Reviewed-on: 541350

--- drivers/Android.mk
+++ drivers/Android.mk
@@ -14,17 +14,18 @@ LOCAL_STATIC_LIBRARIES := \
 LOCAL_SRC_FILES := \
 	$(foreach drv, $(LIBEFIWRAPPER_DRIVERS), \
 		$(subst $(LOCAL_PATH)/,,$(wildcard $(LOCAL_PATH)/$(drv)/*.c)))
-intermediates := $(local-generated-sources-dir)
-GEN := $(intermediates)/drivers.c
+GEN := $(local-generated-sources-dir)/drivers.c
 ifeq ($(LIBEFIWRAPPER_DRIVERS),)
 $(GEN):
+	$(hide) mkdir -p $(dir $@)
 	$(hide)	echo "/* Do not modify this auto-generated file. */" > $@
 	$(hide) echo "#error \"no driver selected.  Cf. LIBEFIWRAPPER_DRIVERS Makefile variable.\"" >> $@
 else
 $(GEN):
+	$(hide) mkdir -p $(dir $@)
 	$(hide) echo "/* Do not modify this auto-generated file. */" > $@
 	$(hide) echo "#include \"ewdrv.h\"" >> $@
-	$(hide) $(foreach drv, $(LIBEFIWRAPPER_DRIVERS), echo "\#include" \"$(drv)/$(drv).h\" >> $@;)
+	$(hide) $(foreach drv, $(LIBEFIWRAPPER_DRIVERS), echo "#include" \"$(drv)/$(drv).h\" >> $@;)
 	$(hide) echo "" >> $@
 	$(hide) echo "static ewdrv_t *drivers[] = {" >> $@
 	$(hide) $(foreach drv, $(LIBEFIWRAPPER_DRIVERS), echo "&"$(drv)_drv, >> $@;)
